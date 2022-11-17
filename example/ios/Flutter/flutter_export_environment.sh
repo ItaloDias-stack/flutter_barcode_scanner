3c0dad4ba9704f501fc5877a5f2e38688fae6123
Thiago Jordani <thiago.jordani@zrobank.com.br>
2022-11-17 14:39:15 -0300

    iOS :: Adição das telas do iOS nativo

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/joao/Documentos/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/joao/Documentos/git/libs/flutter_barcode_scanner/example"
+export "FLUTTER_ROOT=/Users/tbj/Dev/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/tbj/Dev/flutter_barcode_scanner/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
